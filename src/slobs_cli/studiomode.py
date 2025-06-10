020f3760100593b910511e505cda9e3d36255aeb
onyx-and-iris <code@onyxandiris.online>
2025-06-10 19:10:07 +0100

    add studiomode force-transition command
    patch bump

--- src/slobs_cli/studiomode.py
+++ src/slobs_cli/studiomode.py
@@ -98,3 +98,26 @@ async def toggle(ctx: click.Context):
     async with create_task_group() as tg:
         tg.start_soon(conn.background_processing)
         tg.start_soon(_run)
+
+
+@studiomode.command()
+@click.pass_context
+async def force_transition(ctx: click.Context):
+    """Force a transition in studio mode."""
+
+    conn = ctx.obj["connection"]
+    ts = TransitionsService(conn)
+
+    async def _run():
+        current_state = await ts.get_model()
+        if not current_state.studio_mode:
+            conn.close()
+            raise click.Abort(click.style("Studio mode is not enabled.", fg="red"))
+
+        await ts.execute_studio_mode_transition()
+        click.echo("Forced studio mode transition.")
+        conn.close()
+
+    async with create_task_group() as tg:
+        tg.start_soon(conn.background_processing)
+        tg.start_soon(_run)
