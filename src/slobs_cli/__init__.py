dcd34b0213c383959e623602d2a138046122c8b3
onyx-and-iris <code@onyxandiris.online>
2025-06-10 13:26:53 +0100

    implement record command group
    minor bump

--- src/slobs_cli/__init__.py
+++ src/slobs_cli/__init__.py
@@ -1,5 +1,6 @@
 from .cli import cli
+from .record import record
 from .scene import scene
 from .stream import stream
 
-__all__ = ["cli", "scene", "stream"]
+__all__ = ["cli", "scene", "stream", "record"]
