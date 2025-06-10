7a88b2b6ed2ab0918e205bd1bb1920c6c82bba5f
onyx-and-iris <code@onyxandiris.online>
2025-06-10 14:09:36 +0100

    add replaybuffer subcommand group
    minor bump

--- src/slobs_cli/__init__.py
+++ src/slobs_cli/__init__.py
@@ -1,7 +1,8 @@
 from .audio import audio
 from .cli import cli
 from .record import record
+from .replaybuffer import replaybuffer
 from .scene import scene
 from .stream import stream
 
-__all__ = ["cli", "scene", "stream", "record", "audio"]
+__all__ = ["cli", "scene", "stream", "record", "audio", "replaybuffer"]
