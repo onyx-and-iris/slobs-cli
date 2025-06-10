4fc3dc015af3ecdcd9a2c196a32e5229655edfd3
onyx-and-iris <code@onyxandiris.online>
2025-06-10 14:03:26 +0100

    add audio subcommand group
    minor bump

--- src/slobs_cli/__init__.py
+++ src/slobs_cli/__init__.py
@@ -1,6 +1,7 @@
+from .audio import audio
 from .cli import cli
 from .record import record
 from .scene import scene
 from .stream import stream
 
-__all__ = ["cli", "scene", "stream", "record"]
+__all__ = ["cli", "scene", "stream", "record", "audio"]
